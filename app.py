446dca9c0c8065d23b467179cb921758c7f714a2
pericles.savio <pericles.savio@redecredauto.com.br>
2025-08-22 10:55:34 -0300

    1.0

---
+++ app.py
@@ -0,0 +1,95 @@
+import pandas as pd
+from flask import Flask, render_template, request
+from funcoes import RA, informar_atualizacao, load_json_vercel, save_json_vercel, converter_url
+
+app = Flask(__name__)
+ra = RA()
+
+@app.context_processor
+def menu():
+    menu_items = [
+        {"nome": "Página Inicial", "rota": "/"},
+        {"nome": "Adicionar/Atualizar Usuário", "rota": "/usuario"},
+    ]
+    return dict(menu_items=menu_items)
+
+@app.route('/', methods=['GET'])
+def index():
+    ultima_atualizacao = informar_atualizacao('dados_ra.json')
+    dados_ra = load_json_vercel('dados_ra.json')
+    dados_pessoais = load_json_vercel('dados_pessoais.json')
+
+    for i in dados_ra:
+        i['UserPicURL'] = f"https://retroachievements.org{i['UserPic']}"
+
+    df_dados_ra = pd.DataFrame(dados_ra)
+    df_dados_pessoais = pd.DataFrame(dados_pessoais)
+
+    final = pd.merge(df_dados_ra, df_dados_pessoais, left_on='User', right_on='Username', how='right', suffixes=('_ra', '_personal'))
+    final['Ratio'] = round(final['TotalTruePoints']/final['TotalPoints'], 2)
+    final['Ratio'] = final['Ratio'].fillna(0)
+
+    final = final[['Username', 'Nome', 'UserPicURL', 'TotalPoints', 'TotalTruePoints', 'Ratio', 'TotalSoftcorePoints', 'Ano', 'Telefone', 'Dev', 'Youtube']]
+    final.columns = ['Usuário', 'Nome', 'Avatar', 'HardPoints', 'RetroPoints', 'Ratio', 'SoftPoints', 'Ano', 'Telefone', 'Dev', 'Youtube']
+    final.insert(0, '#', range(1, len(final) + 1))
+    final = final.sort_values(by='HardPoints', ascending=False).reset_index(drop=True)
+    final.drop(columns=["Telefone"], inplace=True)
+
+    final = converter_url(final)
+
+    tabela_html = final.to_html(index=False, escape=False) if not df_dados_ra.empty else ""
+    return render_template('index.html', tabela=tabela_html, ultima_atualizacao=ultima_atualizacao)
+
+@app.route('/usuario', methods=['GET', 'POST'])
+def usuario():
+    dados_pessoais = load_json_vercel('dados_pessoais.json')
+    sucesso = False
+    falha = False
+    username = ''
+
+    if request.method == 'POST':
+        username = request.form.get('Username')
+        if ra.get_user_profile(username):
+            nome = request.form.get('Nome')
+            ano_str = request.form.get('Ano', '').strip()
+            telefone = request.form.get('Telefone')
+            dev = request.form.get('Dev') == 'on'
+            youtube = request.form.get('Youtube')
+
+            dados_pessoais = [u for u in dados_pessoais if u.get('Username') != username]
+            dados_pessoais.append({
+                'Username': username,
+                'Nome': nome,
+                'Ano': ano_str,
+                'Telefone': telefone,
+                'Dev': dev,
+                'Youtube': youtube
+            })
+
+            save_json_vercel('dados_pessoais.json', dados_pessoais)
+            sucesso = True
+        else:
+            falha = True
+
+    return render_template('usuario.html', sucesso=sucesso, falha=falha, usuario=username)
+
+@app.route('/atualizar_', methods=['GET'])
+def atualizar():
+    dados_pessoais = load_json_vercel('dados_pessoais.json')
+    membros = []
+    dados_ra = []
+
+    for i in dados_pessoais:
+        membros.append(i['Username'])
+
+    for i in membros:
+        perfil = ra.get_user_profile(i)
+        if perfil:
+            dados_ra.append(perfil)
+
+    save_json_vercel('dados_ra.json', dados_ra)
+    ultima_atualizacao = informar_atualizacao('dados_ra.json')
+    return render_template('atualizar.html', ultima_atualizacao=ultima_atualizacao)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ funcoes.py
@@ -0,0 +1,60 @@
+import json
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+def load_json_vercel(filepath):
+    r = requests.get(f'https://tsyfkkmmojo4ck9c.public.blob.vercel-storage.com/{filepath}')
+    return r.json()
+
+def save_json_vercel(filename, data):
+    json_bytes = json.dumps(data, ensure_ascii=False).encode("utf-8")
+    
+    url = f"https://blob.vercel-storage.com/{filename}"
+    headers = {
+        "Authorization": f"Bearer vercel_blob_rw_tSyfkKMmojo4CK9C_rGbToWtoABXIkZMc5mLcvedmlHbg1T",
+        "Content-Type": "application/json",
+        "x-add-random-suffix": "0",
+        "x-allow-overwrite": "1",
+        "x-cache-control-max-age": "60"
+    }
+    r = requests.put(url, headers=headers, data=json_bytes)
+    return r.json()
+
+def informar_atualizacao(filepath):
+    url = f'https://tsyfkkmmojo4ck9c.public.blob.vercel-storage.com/{filepath}'
+    r = requests.get(url)
+    last_modified = r.headers.get("last-modified")    
+    dt = datetime.strptime(last_modified, "%a, %d %b %Y %H:%M:%S %Z")
+    dt_brasil = dt - timedelta(hours=3)
+    ultima_atualizacao = dt_brasil.strftime("%d/%m/%Y %H:%M:%S")
+    return ultima_atualizacao
+
+def converter_url(df):
+    df['Youtube'] = df['Youtube'].fillna('')
+    df['Youtube'] = df['Youtube'].apply(
+        lambda x: f'<a href="{x}" target="_blank" rel="noopener noreferrer">{x.rsplit("/", 1)[-1]}</a>' if x else ''
+    )
+    df['Avatar'] = df['Avatar'].apply(
+        lambda url: f'<img src="{url}" alt="User Pic" style="max-height:50px;">' if pd.notnull(url) else ''
+    )
+    return df
+
+class RA:
+    def __init__(self, my_user='Teste123Teste', api_key='iOQsyUkclggYi3OFOabPSmNs0Hx5fc2t'):
+        self.my_user = my_user
+        self.api_key = api_key
+        self.base_url = "https://retroachievements.org/API"
+
+    def get_user_profile(self, username):
+        url = f"{self.base_url}/API_GetUserProfile.php"
+        params = {"z": self.my_user, "y": self.api_key, "u": username}
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            if data and not data.get('message'):
+                return data
+        except Exception as e:
+            print(f"Erro RA API: {e}")
+        return None
